aabc6fb661b886128108ee1f009a8e554926c5de
Mary Paz <marayam1@ucenfotec.ac.cr>
2018-07-31 12:44:16 -0600

    Ataque completado

--- bases_of_empires_operaciones.sql
+++ bases_of_empires_operaciones.sql
@@ -6,6 +6,7 @@ CREATE OR REPLACE PACKAGE BOE AS
     PROCEDURE MONITOREAR(P_DETALLADO BOOLEAN DEFAULT FALSE);
     PROCEDURE MEJORAR_DEFENSA(P_REINO REINOS.NOMBRE%TYPE);
     PROCEDURE MEJORAR_ATAQUE(P_REINO REINOS.NOMBRE%TYPE);
+    PROCEDURE ATACAR_REINO(P_REINO_ATQ REINOS.NOMBRE%TYPE, P_REINO_DEF REINOS.NOMBRE%TYPE);
 END BOE; 
 /
 
@@ -186,6 +187,288 @@ CREATE OR REPLACE PACKAGE BODY BOE AS
 		WHERE 
 			UPPER(NOMBRE) = UPPER(P_REINO); 
 		END;
+            
+	    PROCEDURE REINO_ATACADO(P_REINO_DEF REINOS.NOMBRE%TYPE)
+            
+            AS	
+                TRP_CAN TROPAS_POR_REINOS.CANTIDAD%TYPE;
+                TRP_TOR TROPAS_POR_REINOS.CANTIDAD%TYPE;
+                TRP_PIQ TROPAS_POR_REINOS.CANTIDAD%TYPE;
+                TRP_ARQ TROPAS_POR_REINOS.CANTIDAD%TYPE;
+                TRP_CAB TROPAS_POR_REINOS.CANTIDAD%TYPE;
+                TRP_MAG TROPAS_POR_REINOS.CANTIDAD%TYPE;
+                PNS_DEF REINOS.PTS_DEF%TYPE; 	
+                CURSOR CUR_TROPAS_D (P_NOM TROPAS_POR_REINOS.NOMBRE_REINO%TYPE) IS
+                    SELECT NOMBRE_TROPA,
+                        CANTIDAD
+                    FROM TROPAS_POR_REINOS
+                    WHERE NOMBRE_REINO = P_NOM;	
+            BEGIN
+            
+                SELECT PTS_DEF
+                INTO PNS_DEF
+                FROM REINOS
+                WHERE UPPER(NOMBRE) = UPPER(P_REINO_DEF); 
+            
+                PNS_DEF := PNS_DEF - (PNS_DEF*0.1);
+                
+                UPDATE REINOS SET PTS_DEF = PNS_DEF WHERE UPPER(NOMBRE) = UPPER(P_REINO_DEF);
+            
+                FOR REGIS IN CUR_TROPAS_D(P_REINO_DEF) loop
+                    IF(regis.nombre_tropa = 'CAÑON') THEN
+                            TRP_CAN := regis.cantidad - round((regis.cantidad*0.25),2);
+            
+                         UPDATE TROPAS_POR_REINOS
+                             SET CANTIDAD = TRP_CAN
+                             WHERE UPPER(NOMBRE_REINO) = UPPER(P_REINO_DEF) AND 
+                                     UPPER(NOMBRE_TROPA) = 'CAÑON';
+            
+                    ELSIF(regis.nombre_tropa = 'TORRE') THEN
+                        TRP_TOR := regis.cantidad - round((regis.cantidad*0.25),2);
+            
+                         UPDATE TROPAS_POR_REINOS
+                             SET CANTIDAD = TRP_TOR
+                             WHERE UPPER(NOMBRE_REINO) = UPPER(P_REINO_DEF) AND 
+                                     UPPER(NOMBRE_TROPA) = 'TORRE';
+                    ELSIF(regis.nombre_tropa = 'ARQUERA') THEN
+                            TRP_ARQ := regis.cantidad - round((regis.cantidad*0.2),2);
+            
+                         UPDATE TROPAS_POR_REINOS
+                             SET CANTIDAD = TRP_ARQ
+                             WHERE UPPER(NOMBRE_REINO) = UPPER(P_REINO_DEF) AND 
+                                     UPPER(NOMBRE_TROPA) = 'ARQUERA';
+            
+                    ELSIF(regis.nombre_tropa = 'PIQUERO') THEN
+                        TRP_PIQ := regis.cantidad - round((regis.cantidad*0.2),2);
+            
+                         UPDATE TROPAS_POR_REINOS
+                             SET CANTIDAD = TRP_PIQ
+                             WHERE UPPER(NOMBRE_REINO) = UPPER(P_REINO_DEF) AND 
+                                     UPPER(NOMBRE_TROPA) = 'PIQUERO';
+            
+                    ELSIF(regis.nombre_tropa = 'CABALLERO') THEN
+                        TRP_CAB := regis.cantidad - round((regis.cantidad*0.2),2);
+            
+                         UPDATE TROPAS_POR_REINOS
+                             SET CANTIDAD = TRP_CAB
+                             WHERE UPPER(NOMBRE_REINO) = UPPER(P_REINO_DEF) AND 
+                                     UPPER(NOMBRE_TROPA) = 'CABALLERO';
+            
+                    ELSIF(regis.nombre_tropa = 'MAGO') THEN
+                        TRP_MAG := regis.cantidad - round((regis.cantidad*0.2),2);
+            
+                         UPDATE TROPAS_POR_REINOS
+                             SET CANTIDAD = TRP_MAG
+                             WHERE UPPER(NOMBRE_REINO) = UPPER(P_REINO_DEF) AND 
+                                     UPPER(NOMBRE_TROPA) = 'MAGO';		
+                    END IF;	 
+                END LOOP;
+                
+            END;
+            
+            PROCEDURE ATAQUE_EXITOSO(P_REINO_ATQ REINOS.NOMBRE%TYPE,
+                P_REINO_DEF REINOS.NOMBRE%TYPE)
+            AS	
+                PNS_ATQ REINOS.PTS_DEF%TYPE;
+                TRP_PIQ TROPAS_POR_REINOS.CANTIDAD%TYPE;
+                TRP_ARQ TROPAS_POR_REINOS.CANTIDAD%TYPE;
+                TRP_CAB TROPAS_POR_REINOS.CANTIDAD%TYPE;
+                TRP_MAG TROPAS_POR_REINOS.CANTIDAD%TYPE;
+                CANT_ORO RECURSOS_POR_REINOS.CANTIDAD%TYPE;
+                CANT_MAD RECURSOS_POR_REINOS.CANTIDAD%TYPE;
+                CANT_HIE RECURSOS_POR_REINOS.CANTIDAD%TYPE;
+            
+                CURSOR CUR_TROPAS_A (P_NOM TROPAS_POR_REINOS.NOMBRE_REINO%TYPE) IS
+                    SELECT NOMBRE_TROPA,
+                        CANTIDAD
+                    FROM TROPAS_POR_REINOS
+                    WHERE NOMBRE_REINO = P_NOM;
+                CURSOR CUR_RECUR_REINO (P_NOM RECURSOS_POR_REINOS.NOMBRE_REINO%TYPE) IS
+                    SELECT NOMBRE_RECURSO,
+                        CANTIDAD
+                    FROM RECURSOS_POR_REINOS
+                    WHERE NOMBRE_REINO = P_NOM;
+            
+            BEGIN
+                REINO_ATACADO(P_REINO_DEF);
+            
+                SELECT PTS_ATQ
+                INTO PNS_ATQ 
+                FROM REINOS
+                WHERE UPPER(NOMBRE) = UPPER(P_REINO_ATQ); 
+            
+                PNS_ATQ := PNS_ATQ - (PNS_ATQ *0.5);	
+            
+                UPDATE REINOS 
+                SET PTS_ATQ = PNS_ATQ 
+                WHERE UPPER(NOMBRE) = UPPER(P_REINO_ATQ);
+            
+                FOR REGIS IN CUR_TROPAS_A(P_REINO_ATQ) loop
+                    IF(regis.nombre_tropa = 'ARQUERA') THEN
+                            TRP_ARQ := regis.cantidad - round((regis.cantidad*0.3),2);
+            
+                         UPDATE TROPAS_POR_REINOS
+                             SET CANTIDAD = TRP_ARQ
+                             WHERE UPPER(NOMBRE_REINO) = UPPER(P_REINO_ATQ) AND 
+                                     UPPER(NOMBRE_TROPA) = 'ARQUERA';
+            
+                    ELSIF(regis.nombre_tropa = 'PIQUERO') THEN
+                        TRP_PIQ := regis.cantidad - round((regis.cantidad*0.3),2);
+            
+                         UPDATE TROPAS_POR_REINOS
+                             SET CANTIDAD = TRP_PIQ
+                             WHERE UPPER(NOMBRE_REINO) = UPPER(P_REINO_ATQ) AND 
+                                     UPPER(NOMBRE_TROPA) = 'PIQUERO';
+            
+                    ELSIF(regis.nombre_tropa = 'CABALLERO') THEN
+                        TRP_CAB := regis.cantidad - round((regis.cantidad*0.3),2);
+            
+                         UPDATE TROPAS_POR_REINOS
+                             SET CANTIDAD = TRP_CAB
+                             WHERE UPPER(NOMBRE_REINO) = UPPER(P_REINO_ATQ) AND 
+                                     UPPER(NOMBRE_TROPA) = 'CABALLERO';
+            
+                    ELSIF(regis.nombre_tropa = 'MAGO') THEN
+                        TRP_MAG := regis.cantidad - round((regis.cantidad*0.3),2);
+            
+                         UPDATE TROPAS_POR_REINOS
+                             SET CANTIDAD = TRP_MAG
+                             WHERE UPPER(NOMBRE_REINO) = UPPER(P_REINO_ATQ) AND 
+                                     UPPER(NOMBRE_TROPA) = 'MAGO';
+                    END IF;	 
+                END LOOP;
+            
+                FOR REGIS IN CUR_RECUR_REINO(P_REINO_DEF) loop
+                    IF(REGIS.NOMBRE_RECURSO = 'ORO') THEN
+                            CANT_ORO := round((regis.cantidad*0.65),2);
+            
+                         UPDATE RECURSOS_POR_REINOS
+                             SET CANTIDAD = (regis.cantidad - round((regis.cantidad*0.65),2))
+                             WHERE UPPER(NOMBRE_REINO) = UPPER(P_REINO_DEF) AND 
+                                     UPPER(NOMBRE_RECURSO) = 'ORO';
+            
+                    ELSIF(REGIS.NOMBRE_RECURSO = 'MADERA') THEN
+                            CANT_MAD := round((regis.cantidad*0.65),2);
+            
+                         UPDATE RECURSOS_POR_REINOS
+                             SET CANTIDAD = (regis.cantidad - round((regis.cantidad*0.65),2))
+                             WHERE UPPER(NOMBRE_REINO) = UPPER(P_REINO_DEF) AND 
+                                     UPPER(NOMBRE_RECURSO) = 'MADERA';
+            
+                    ELSIF(REGIS.NOMBRE_RECURSO = 'HIERRO') THEN
+                            CANT_HIE := round((regis.cantidad*0.65),2);
+            
+                         UPDATE RECURSOS_POR_REINOS
+                             SET CANTIDAD = (regis.cantidad - round((regis.cantidad*0.65),2))
+                             WHERE UPPER(NOMBRE_REINO) = UPPER(P_REINO_DEF) AND 
+                                     UPPER(NOMBRE_RECURSO) = 'HIERRO';
+                    END IF;	 
+                END LOOP;
+            
+                FOR REGIS IN CUR_RECUR_REINO(P_REINO_ATQ) loop
+                    IF(REGIS.NOMBRE_RECURSO = 'ORO') THEN
+            
+                         UPDATE RECURSOS_POR_REINOS
+                             SET CANTIDAD = (regis.cantidad + CANT_ORO)
+                             WHERE UPPER(NOMBRE_REINO) = UPPER(P_REINO_ATQ) AND 
+                                     UPPER(NOMBRE_RECURSO) = 'ORO';
+            
+                    ELSIF(REGIS.NOMBRE_RECURSO = 'MADERA') THEN
+            
+                         UPDATE RECURSOS_POR_REINOS
+                             SET CANTIDAD = (regis.cantidad + CANT_MAD)
+                             WHERE UPPER(NOMBRE_REINO) = UPPER(P_REINO_ATQ) AND 
+                                     UPPER(NOMBRE_RECURSO) = 'MADERA';
+            
+                    ELSIF(REGIS.NOMBRE_RECURSO = 'HIERRO') THEN
+            
+                         UPDATE RECURSOS_POR_REINOS
+                             SET CANTIDAD = (regis.cantidad + CANT_HIE)
+                             WHERE UPPER(NOMBRE_REINO) = UPPER(P_REINO_ATQ) AND 
+                                     UPPER(NOMBRE_RECURSO) = 'HIERRO';
+                    END IF;	 
+                END LOOP;
+            
+            END;
+            
+            PROCEDURE ATAQUE_FALLIDO(P_REINO_ATQ REINOS.NOMBRE%TYPE,
+                P_REINO_DEF REINOS.NOMBRE%TYPE)
+            AS	
+                TRP_PIQ TROPAS_POR_REINOS.CANTIDAD%TYPE;
+                TRP_ARQ TROPAS_POR_REINOS.CANTIDAD%TYPE;
+                TRP_CAB TROPAS_POR_REINOS.CANTIDAD%TYPE;
+                TRP_MAG TROPAS_POR_REINOS.CANTIDAD%TYPE;
+                PNS_ATQ REINOS.PTS_DEF%TYPE;
+                CANT_ORO  RECURSOS_POR_REINOS.CANTIDAD%TYPE; 
+                CURSOR CUR_TROPAS_A (P_NOM TROPAS_POR_REINOS.NOMBRE_REINO%TYPE) IS
+                    SELECT NOMBRE_TROPA,
+                        CANTIDAD
+                    FROM TROPAS_POR_REINOS
+                    WHERE NOMBRE_REINO = P_NOM;	
+            
+            BEGIN	
+                REINO_ATACADO(P_REINO_DEF);
+            
+                SELECT PTS_ATQ
+                INTO PNS_ATQ 
+                FROM REINOS
+                WHERE UPPER(NOMBRE) = UPPER(P_REINO_ATQ); 
+            
+                SELECT CANTIDAD
+                INTO CANT_ORO
+                FROM RECURSOS_POR_REINOS
+                WHERE UPPER(NOMBRE_REINO) = UPPER(P_REINO_ATQ) AND UPPER(NOMBRE_RECURSO) = 'ORO'; 
+            
+                PNS_ATQ := PNS_ATQ - (PNS_ATQ *0.2);
+                CANT_ORO := (CANT_ORO*0.3);	
+            
+                UPDATE REINOS 
+                SET PTS_ATQ = PNS_ATQ 
+                WHERE UPPER(NOMBRE) = UPPER(P_REINO_ATQ);
+            
+                UPDATE RECURSOS_POR_REINOS
+                SET CANTIDAD = (CANTIDAD - CANT_ORO)
+                WHERE UPPER(NOMBRE_REINO) = UPPER(P_REINO_ATQ) AND UPPER(NOMBRE_RECURSO) = 'ORO';
+            
+                UPDATE RECURSOS_POR_REINOS
+                SET CANTIDAD = (CANTIDAD + CANT_ORO)
+                WHERE UPPER(NOMBRE_REINO) = UPPER(P_REINO_DEF) AND UPPER(NOMBRE_RECURSO) = 'ORO';	
+            
+                FOR REGIS IN CUR_TROPAS_A(P_REINO_ATQ) loop
+                    IF(regis.nombre_tropa = 'ARQUERA') THEN
+                            TRP_ARQ := regis.cantidad - round((regis.cantidad*0.4),2);
+            
+                         UPDATE TROPAS_POR_REINOS
+                             SET CANTIDAD = TRP_ARQ
+                             WHERE UPPER(NOMBRE_REINO) = UPPER(P_REINO_ATQ) AND 
+                                     UPPER(NOMBRE_TROPA) = 'ARQUERA';
+            
+                    ELSIF(regis.nombre_tropa = 'PIQUERO') THEN
+                        TRP_PIQ := regis.cantidad - round((regis.cantidad*0.4),2);
+            
+                         UPDATE TROPAS_POR_REINOS
+                             SET CANTIDAD = TRP_PIQ
+                             WHERE UPPER(NOMBRE_REINO) = UPPER(P_REINO_ATQ) AND 
+                                     UPPER(NOMBRE_TROPA) = 'PIQUERO';
+            
+                    ELSIF(regis.nombre_tropa = 'CABALLERO') THEN
+                        TRP_CAB := regis.cantidad - round((regis.cantidad*0.4),2);
+            
+                         UPDATE TROPAS_POR_REINOS
+                             SET CANTIDAD = TRP_CAB
+                             WHERE UPPER(NOMBRE_REINO) = UPPER(P_REINO_ATQ) AND 
+                                     UPPER(NOMBRE_TROPA) = 'CABALLERO';
+            
+                    ELSIF(regis.nombre_tropa = 'MAGO') THEN
+                        TRP_MAG := regis.cantidad - round((regis.cantidad*0.4),2);
+            
+                         UPDATE TROPAS_POR_REINOS
+                             SET CANTIDAD = TRP_MAG
+                             WHERE UPPER(NOMBRE_REINO) = UPPER(P_REINO_ATQ) AND 
+                                     UPPER(NOMBRE_TROPA) = 'MAGO';
+                    END IF;	 
+                END LOOP;	 
+            END;
 
             PROCEDURE COMPRAR(P_RECURSO RECURSOS.NOMBRE%TYPE, P_CANTIDAD RECURSOS.RESERVA%TYPE, P_REINO REINOS.NOMBRE%TYPE) AS BEGIN
                 INTERCAMBIAR(P_RECURSO, P_CANTIDAD, P_REINO);
@@ -325,5 +608,82 @@ CREATE OR REPLACE PACKAGE BODY BOE AS
                     AGREGAR_CORONAS(P_REINO,5);
                     AGREGAR_ENTRADA_BITACORA (P_REINO,'M+A');
                 END;
+
+		PROCEDURE ATACAR_REINO (
+            P_REINO_ATQ REINOS.NOMBRE%TYPE,
+            P_REINO_DEF REINOS.NOMBRE%TYPE)
+        AS
+            ORO_REINO RECURSOS_POR_REINOS.NOMBRE_RECURSO%TYPE := 'ORO';
+            CANT_ORO  RECURSOS_POR_REINOS.CANTIDAD%TYPE;
+            PTS_TRP_A TROPAS.PUNTOS%TYPE := 0;
+            PTS_TRP_D TROPAS.PUNTOS%TYPE := 0;
+            PNS_ATQ REINOS.PTS_ATQ%TYPE;
+            PNS_DEF REINOS.PTS_DEF%TYPE;
+            CURSOR CUR_TROPAS_A (P_NOM TROPAS_POR_REINOS.NOMBRE_REINO%TYPE) IS
+                SELECT NOMBRE_TROPA,
+                    CANTIDAD
+                FROM TROPAS_POR_REINOS
+                WHERE NOMBRE_REINO = P_NOM;
+            CURSOR CUR_TROPAS_D (P_NOM TROPAS_POR_REINOS.NOMBRE_REINO%TYPE) IS
+                SELECT NOMBRE_TROPA,
+                    CANTIDAD
+                FROM TROPAS_POR_REINOS
+                WHERE NOMBRE_REINO = P_NOM;
+        BEGIN
+            SELECT CANTIDAD
+            INTO CANT_ORO
+            FROM RECURSOS_POR_REINOS
+            WHERE UPPER(NOMBRE_REINO) = UPPER(P_REINO_ATQ) AND NOMBRE_RECURSO = ORO_REINO;	
+            
+            SELECT PTS_ATQ
+            INTO PNS_ATQ
+            FROM REINOS
+            WHERE UPPER(NOMBRE) = UPPER(P_REINO_ATQ);
+        
+            SELECT PTS_DEF
+            INTO PNS_DEF
+            FROM REINOS
+            WHERE UPPER(NOMBRE) = UPPER(P_REINO_DEF);
+        
+            IF(CANT_ORO < 1000) THEN
+                BOE_ERRORS.RAISE_BOE_ERR(BOE_ERRORS.REC_INSUF_PARA_ATQ_NUM);
+            END IF;
+        
+            TRAMITE_RESERVA(ORO_REINO, 1000, P_REINO_ATQ, FALSE);
+        
+            FOR REGIS IN CUR_TROPAS_A(P_REINO_ATQ) loop
+                IF(regis.nombre_tropa = 'ARQUERA') THEN
+                        PTS_TRP_A := PTS_TRP_A + round((regis.cantidad*20*0.8),2);
+                ELSIF(regis.nombre_tropa = 'PIQUERO') THEN
+                    PTS_TRP_A := PTS_TRP_A + round((regis.cantidad*30*0.8),2);
+                ELSIF(regis.nombre_tropa = 'CABALLERO') THEN
+                    PTS_TRP_A := PTS_TRP_A + round((regis.cantidad*50*0.8),2);
+                ELSIF(regis.nombre_tropa = 'MAGO') THEN
+                    PTS_TRP_A := PTS_TRP_A + round((regis.cantidad*40*0.8),2);
+                END IF;	 
+            end loop;
+	
+            PNS_ATQ := ROUND((PNS_ATQ*0.6),2) + PTS_TRP_A;	
+            
+            FOR REGIS IN CUR_TROPAS_D(P_REINO_DEF) loop
+                IF(regis.nombre_tropa = 'CAÑON') THEN
+                        PTS_TRP_D := PTS_TRP_D + round((regis.cantidad*450),2);
+                ELSIF(regis.nombre_tropa = 'TORRE') THEN
+                    PTS_TRP_D := PTS_TRP_D + round((regis.cantidad*650),2);
+                END IF;	 
+            end loop;		
+            
+            PNS_DEF := ROUND((PNS_DEF*0.7),2) + PTS_TRP_D;
+        
+            IF (PNS_ATQ > PNS_DEF) THEN
+                ATAQUE_EXITOSO(P_REINO_ATQ, P_REINO_DEF);
+            ELSE
+                ATAQUE_FALLIDO(P_REINO_ATQ, P_REINO_DEF);
+            END IF;
+            
+            AGREGAR_CORONAS(P_REINO_ATQ,2);
+            AGREGAR_ENTRADA_BITACORA (P_REINO_ATQ,'ATK');
+	
+        END;
 END BOE; 
 /
